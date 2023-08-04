e7a3b70cac63b9e79dc0fcbce183aad879dddbf4
Gerroar <germanariasrodriguez@gmail.com>
2023-08-04 16:14:49 +0200

    Building our React app

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,21 @@
-const config = { 
-    
-}
+const config = {
+  // Backend config
+  STRIPE_KEY: "pk_test_51Nb0KSBfZ8OzKEApH6JJUZoG1WB4IM7YsleduKYrEdvQ3VZkZJRDPgY1thmgNO9zMQWUQZJh1G5hoj1yhVAXQYnZ00sBmNgCEC",
+  MAX_ATTACHMENT_SIZE: 5000000,
+  s3: {
+    REGION: process.env.REACT_APP_REGION,
+    BUCKET: process.env.REACT_APP_BUCKET,
+  },
+  apiGateway: {
+    REGION: process.env.REACT_APP_REGION,
+    URL: process.env.REACT_APP_API_URL,
+  },
+  cognito: {
+    REGION: process.env.REACT_APP_REGION,
+    USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID,
+    APP_CLIENT_ID: process.env.REACT_APP_USER_POOL_CLIENT_ID,
+    IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL_ID,
+  },
+};
+
+export default config;
