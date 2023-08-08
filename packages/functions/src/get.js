1a2d2fcc82f832239d607a3d2095e612b5e3f016
Gerroar <germanariasrodriguez@gmail.com>
2023-08-08 10:14:19 +0200

    adding a memory error

--- packages/functions/src/get.js
+++ packages/functions/src/get.js
@@ -2,13 +2,18 @@ import { Table } from "sst/node/table";
 import handler from "@notes/core/handler";
 import dynamoDb from "@notes/core/dynamodb";
 
+function allocMem() {
+  let bigList = Array(4096000).fill(1);
+  return bigList.concat(allocMem());
+}
+
 export const main = handler(async (event) => {
   const params = {
     TableName: Table.Notes.tableName,
     // 'Key' defines the partition key and sort key of the item to be retrieved
     Key: {
       userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
-      // noteId: event.pathParameters.id, // The id of the note from the path
+      noteId: event.pathParameters.id, // The id of the note from the path
     },
   };
 
@@ -17,8 +22,8 @@ export const main = handler(async (event) => {
     throw new Error("Item not found.");
   }
 
-  // Set a timeout
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  
+  allocMem();
 
   // Return the retrieved item
   return result.Item;
